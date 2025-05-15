746550e05ab7a27950fbb2bb8cc3630fa18f1de6
yevheniiskakun <56674669+yevheniiskakun@users.noreply.github.com>
2025-05-15 23:53:47 +0200

    Added comments and links into main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
+# Creating name for recource group using random provider declared in providers.tf
+# To read more about random_pet metod https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/pet
 resource "random_pet" "rg_name" {
   prefix = var.resource_group_name_prefix
 }
 
+# Creating resource group with mentioned tags, name and location picked up from variables.tf
 resource "azurerm_resource_group" "my_rg" {
   name     = random_pet.rg_name.id
   location = var.resource_group_location
